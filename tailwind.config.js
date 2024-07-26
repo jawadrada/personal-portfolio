bf828bc3ab716fa6668324de20b2140e9a12d0a9
jawadrada <jawadrada18@gmail.com>
2024-07-26 01:16:22 -0400

    Re configured files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      worksans: ['Work Sans', 'sans-serif'],
+      sans: ['lato', 'Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      animation: {
+        'bounce-slow': 'bounce-slow 3s ease-in-out infinite',
+      },
+      keyframes: {
+        'bounce-slow': {
+          '0%, 100%': { transform: 'translateY(-10%)', animationTimingFunction: 'ease-in-out' },
+          '50%': { transform: 'translateY(0)', animationTimingFunction: 'ease-in-out' },
+        },
+      },
+    }
   },
   plugins: [],
 }
-
