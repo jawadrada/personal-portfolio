efea23a1bd7348df82ebd18f4fa90ba64b74cfbe
jawadrada <jawadrada18@gmail.com>
2024-07-26 01:16:42 -0400

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import '@fontsource/lato';
 
 function Footer() {
   return (
     <>
+      <footer className="mt-3 p-4 font-lato text-black text-center">
+        <p>Designed & Built by <span className="font-semibold">Jawad Rada</span> &copy; 2024</p>
+      </footer>
     </>
   );
 }
