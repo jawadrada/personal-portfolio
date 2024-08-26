1048aaf11de3d888f934253fd6a00930c2b6e4e3
Jawad Rada <103535961+jawadrada@users.noreply.github.com>
2024-08-25 21:41:06 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,7 +22,7 @@ function NavBar() {
         </div>
 
         <div className={`lg:flex lg:items-center ${isOpen ? "block" : "hidden"}`}>
-          <a href="https://drive.google.com/file/d/1tJr7EjpOXKxJ8QPTUtnX7T01HiI7MZ3S/view?usp=drive_link" target="_blank" className="block mt-4 lg:inline-block lg:mt-0 hover:text-black text-[#555] mr-10">Resume</a>
+          <a href="https://drive.google.com/file/d/1573LZUWGLC3yE48K2POI9Q3I22ue9jiP/view?usp=drive_link" target="_blank" className="block mt-4 lg:inline-block lg:mt-0 hover:text-black text-[#555] mr-10">Resume</a>
           <a href="https://github.com/jawadrada?tab=overview&from=2024-07-01&to=2024-07-23" target="_blank" className="block mt-4 lg:inline-block lg:mt-0 hover:text-black text-[#555] mr-10">GitHub</a>
           <a href="https://www.linkedin.com/in/jawad-rada/" target="_blank" className="block mt-4 lg:inline-block lg:mt-0 hover:text-black text-[#555] mr-10">LinkedIn</a>
         </div>
